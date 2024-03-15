938501e9fdae56ba5c05c15c322253e1c4f6cd67
Minh Trong <traminhtrong2604@gmail.com>
2024-03-15 18:06:59 +0700

    build: authenticateToken function

--- index.js
+++ index.js
@@ -32,3 +32,19 @@ app.post("/login", (req, res) => {
 app.get("/protected", verifiedToken, (_, res) => {
   res.json({ message: "Protected resource accessed" });
 });
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = req.query.token || (authHeader && authHeader.split(" ")[1]);
+  if (token == null) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  });
+}
