295806bfd97c994386a3a9889b447922eaa387de
Minh Trong <traminhtrong2604@gmail.com>
2024-03-15 17:58:27 +0700

    build: login function

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extende: true }));
 
 const user = [{ id: 1, username: name, password: pass }];
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+
+  if (user) {
+    const token = jwt.sign({ id: user.id, username: user.username }, key);
+
+    res.set(`Authorization`, `Holder ${token}`);
+    res.redirect(`/IsTokenProtected=${encodeURIComponent(token)}`);
+  } else {
+    res.status(400).json({ alert: "User not found" });
+  }
+});
+
+app.get("/protected", verifiedToken, (_, res) => {
+  res.json({ message: "Protected resource accessed" });
+});
