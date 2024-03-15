fa826d929ad09cc4525ddbec9555e0c51b0fa53a
Minh Trong <traminhtrong2604@gmail.com>
2024-03-15 20:31:15 +0700

    fix: function and variable typo

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const pass = process.env.USER_PASSWORD;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extende: true }));
+app.use(express.urlencoded({ extended: true }));
 
-const user = [{ id: 1, username: name, password: pass }];
+const users = [{ id: 1, username: name, password: pass }];
 
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
@@ -24,16 +24,16 @@ app.post("/login", (req, res) => {
     res.set(`Authorization`, `Holder ${token}`);
     res.redirect(`/IsTokenProtected=${encodeURIComponent(token)}`);
   } else {
-    res.status(400).json({ alert: "User not found" });
+    res.status(401).json({ alert: "User not found" });
   }
 });
 
-app.get("/protected", verifiedToken, (_, res) => {
+app.get("/IsTokenProtected", verifiedToken, (_, res) => {
   res.json({ message: "Protected resource accessed" });
 });
 
 function verifiedToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
+  const authHeader = req.headers["Authorization"];
   const token = req.query.token || (authHeader && authHeader.split(" ")[1]);
   if (token == null) {
     return res.sendStatus(401);
