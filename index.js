eb418f6e63dbdb963c4adacaf156145a01c1a0b6
Minh Trong <traminhtrong2604@gmail.com>
2024-03-19 13:16:07 +0700

    build: increase security with pbkdf2

--- index.js
+++ index.js
@@ -1,34 +1,77 @@
 require("dotenv").config();
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const key = process.env.PRIVATE_KEY;
 const name = process.env.USER_NAME;
 const pass = process.env.USER_PASSWORD;
+const salt = process.env.USER_SALT;
+const iterations = 10000;
+const keylen = 32;
+const digest = "sha512";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const users = [{ id: 1, username: name, password: pass }];
+async function hashedPassword(password, salt) {
+  return new Promise((resolve, reject) => {
+    if (!salt || typeof salt !== "string") {
+      reject(new Error("Salt is undefined or not a string"));
+      return;
+    }
 
-app.post("/login", (req, res) => {
+    crypto.pbkdf2(
+      password,
+      salt,
+      iterations,
+      keylen,
+      digest,
+      (err, deriveKey) => {
+        if (err) reject(err);
+        else resolve(deriveKey.toString("hex"));
+      }
+    );
+  });
+}
+
+async function InitializedUsers() {
+  const hashedpass = await hashedPassword(pass, salt);
+  return [
+    {
+      id: 1,
+      username: name,
+      password: hashedpass,
+      salt: salt,
+    },
+  ];
+}
+
+app.post("/login", async (req, res) => {
   console.log("Received request body:", req.body);
   const { username, password } = req.body;
-  const user = users.find(
-    (u) => u.username === username && u.password === password
-  );
+  try {
+    const users = await InitializedUsers();
+    const hashedpass = await hashedPassword(password, salt);
 
-  if (user) {
-    const token = jwt.sign({ id: user.id, username: user.username }, key);
+    const user = users.find(
+      (u) => u.username === username && u.password === hashedpass
+    );
+    if (user) {
+      const token = jwt.sign({ id: user.id, username: user.username }, key);
 
-    res.header(`Authorization`, `Holder ${token}`);
-    res.redirect(`/IsTokenProtected?token=${encodeURIComponent(token)}`);
-  } else {
-    const isUserExist = users.find((u) => u.username === username);
-    res
-      .status(401)
-      .json({ alert: isUserExist ? "Wrong password" : "User not found" });
+      res.header(`Authorization`, `Holder ${token}`);
+      res.redirect(`/IsTokenProtected?token=${encodeURIComponent(token)}`);
+    } else {
+      const isUserExist = users.find((u) => u.username === username);
+      res
+        .status(401)
+        .json({ alert: isUserExist ? "Wrong password" : "User not found" });
+    }
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).json({ alert: "Internal server error" });
   }
 });
 
