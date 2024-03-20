4949cb7af917fad64225c39f839605ba40b5850a
Minh Trong <traminhtrong2604@gmail.com>
2024-03-20 17:42:46 +0700

    build: key and address generation from mnemonic generation

--- KeyGenerate.js
+++ KeyGenerate.js
@@ -1,10 +1,34 @@
 const bip39 = require("bip39");
+const { DirectSecp256k1HdWallet } = require("@cosmjs/proto-signing");
+
+// Generate key
+
+async function KeyGen(Generate_mnemonic) {
+  //const hdPath = "m/44'/118'/0'/0/0";
+  try {
+    // this function already generate the seed
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
+      Generate_mnemonic,
+      {
+        prefix: "thasa",
+      }
+    );
+
+    const [{ pubkey, address }] = await wallet.getAccounts();
+    console.log("Public key:", pubkey.toString());
+    console.log("Address:", address);
+  } catch (error) {
+    console.error("Error generating key:", error);
+  }
+}
 
 // Generate a random mnemonic (12 words by default)
-const mnemonic = bip39.generateMnemonic();
+async function generateMnemonicAndKey() {
+  const mnemonic = await bip39.generateMnemonic();
+  console.log("Generated mnemonic:", mnemonic);
 
-console.log("Generated mnemonic:", mnemonic);
+  // Testing
+  await KeyGen(mnemonic);
+}
 
-// Generate a seed from mnemonic
-const seed = bip39.mnemonicToSeedSync(mnemonic);
-console.log("Private Key generated:", seed);
+generateMnemonicAndKey();
