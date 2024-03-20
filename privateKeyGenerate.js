ad7a303a827abe719fcf68d97a1ebdcdc8987862
Minh Trong <traminhtrong2604@gmail.com>
2024-03-20 13:07:39 +0700

    build: using ethers.js to generate private key

--- privateKeyGenerate.js
+++ privateKeyGenerate.js
@@ -1,6 +1,11 @@
 const bip39 = require("bip39");
+const ethers = require("ethers");
 
 // Generate a random mnemonic (12 words by default)
 const mnemonic = bip39.generateMnemonic();
 
 console.log("Generated mnemonic:", mnemonic);
+
+// Generate a private key from mnemonic
+const mnemonicWallet = ethers.Wallet.fromPhrase(mnemonic);
+console.log(mnemonicWallet.privateKey);
