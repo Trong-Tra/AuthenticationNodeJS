753488031f856c806bb461255b80d37b1e07ae69
Minh Trong <traminhtrong2604@gmail.com>
2024-03-20 14:24:20 +0700

    build: authenticate password with bcryt

--- authentication.js
+++ authentication.js
@@ -1,49 +1,50 @@
 require("dotenv").config();
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const crypto = require("crypto");
+const bcrypt = require("bcrypt");
 
 const key = process.env.PRIVATE_KEY;
 const name = process.env.USER_NAME;
 const pass = process.env.USER_PASSWORD;
-const salt = process.env.USER_SALT;
-const iterations = 10000;
-const keylen = 32;
-const digest = "sha512";
+// const salt = process.env.USER_SALT;
+// const iterations = 10000;
+// const keylen = 32;
+// const digest = "sha512";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-async function hashedPassword(password, salt) {
-  return new Promise((resolve, reject) => {
-    if (!salt || typeof salt !== "string") {
-      reject(new Error("Salt is undefined or not a string"));
-      return;
-    }
+// async function hashedPassword(password, salt) {
+//   return new Promise((resolve, reject) => {
+//     if (!salt || typeof salt !== "string") {
+//       reject(new Error("Salt is undefined or not a string"));
+//       return;
+//     }
 
-    crypto.pbkdf2(
-      password,
-      salt,
-      iterations,
-      keylen,
-      digest,
-      (err, deriveKey) => {
-        if (err) reject(err);
-        else resolve(deriveKey.toString("hex"));
-      }
-    );
-  });
-}
+//     crypto.pbkdf2(
+//       password,
+//       salt,
+//       iterations,
+//       keylen,
+//       digest,
+//       (err, deriveKey) => {
+//         if (err) reject(err);
+//         else resolve(deriveKey.toString("hex"));
+//       }
+//     );
+//   });
+// }
 
 async function InitializedUsers() {
-  const hashedpass = await hashedPassword(pass, salt);
+  const saltRounds = 10;
+  const hashedpass = await bcrypt.hash(pass, saltRounds);
   return [
     {
       id: 1,
       username: name,
       password: hashedpass,
-      salt: salt,
+      // salt: salt,
     },
   ];
 }
@@ -53,21 +54,21 @@ app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
     const users = await InitializedUsers();
-    const hashedpass = await hashedPassword(password, salt);
+    // const hashedpass = await hashedPassword(password, salt);
 
-    const user = users.find(
-      (u) => u.username === username && u.password === hashedpass
-    );
+    const user = users.find((u) => u.username === username);
     if (user) {
-      const token = jwt.sign({ id: user.id, username: user.username }, key);
+      const match = await bcrypt.compare(password, user.password);
+      if (match) {
+        const token = jwt.sign({ id: user.id, username: user.username }, key);
 
-      res.header(`Authorization`, `Holder ${token}`);
-      res.redirect(`/IsTokenProtected?token=${encodeURIComponent(token)}`);
+        res.header(`Authorization`, `Holder ${token}`);
+        res.redirect(`/IsTokenProtected?token=${encodeURIComponent(token)}`);
+      } else {
+        res.status(401).json({ alert: "Wrong password" });
+      }
     } else {
-      const isUserExist = users.find((u) => u.username === username);
-      res
-        .status(401)
-        .json({ alert: isUserExist ? "Wrong password" : "User not found" });
+      res.status(401).json({ alert: "User not found" });
     }
   } catch (err) {
     console.error("Error:", err);
