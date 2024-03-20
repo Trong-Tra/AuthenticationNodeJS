8a1509a4728a2854d3bb630551cedbdfaf876ba0
Minh Trong <traminhtrong2604@gmail.com>
2024-03-20 13:17:02 +0700

    build: generated seed from mnemonic

--- privateKeyGenerate.js
+++ privateKeyGenerate.js
@@ -1,11 +1,10 @@
 const bip39 = require("bip39");
-const ethers = require("ethers");
 
 // Generate a random mnemonic (12 words by default)
 const mnemonic = bip39.generateMnemonic();
 
 console.log("Generated mnemonic:", mnemonic);
 
-// Generate a private key from mnemonic
-const mnemonicWallet = ethers.Wallet.fromPhrase(mnemonic);
-console.log(mnemonicWallet.privateKey);
+// Generate a seed from mnemonic
+const seed = bip39.mnemonicToSeedSync(mnemonic);
+console.log("Private Key generated:", seed);
